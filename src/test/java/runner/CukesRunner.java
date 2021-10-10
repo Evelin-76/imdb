2cc38593661b7ecc77d9ac21c40f73bcf4203794
Evelin <evelindacanovas@gmail.com>
2021-10-10 09:45:35 +0200

    Runner package created CukesRunner FailedTestRunner

---
+++ src/test/java/runner/CukesRunner.java
@@ -0,0 +1,17 @@
+package runner;
+
+import io.cucumber.junit.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "src/test/java/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+
+public class CukesRunner {
+
+}
